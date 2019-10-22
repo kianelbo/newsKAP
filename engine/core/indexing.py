15d3cc2e847d77337cad842e14eb86da09a2efb7
Kian Eliasi <kian.elbo@gmail.com>
2019-10-22 23:42:22 +0330

    pos. index and basic query parsing

--- engine/core/indexing.py
+++ engine/core/indexing.py
@@ -25,14 +25,15 @@ if __name__ == "__main__":
 
     sheet = xlrd.open_workbook(dataset_path).sheet_by_index(0)
 
-    vocab = {}
+    postings_list = {}
     for row in range(1, sheet.nrows):
+        token_pos = 1
         content = BeautifulSoup(sheet.row_values(row, 5)[0], features="html.parser").get_text()
         for word in content.split():
+            token_pos += 1
             cleaned_token = clean_token(word)
             if cleaned_token and cleaned_token not in stop_words:
-                # print(cleaned_word)
-                vocab.setdefault(cleaned_token, []).append(row)
+                postings_list.setdefault(cleaned_token, {}).setdefault(row, []).append(token_pos)
 
     with open(index_file_path, 'wb') as index_file:
-        pickle.dump(vocab, index_file)
+        pickle.dump(postings_list, index_file)
--- engine/core/queries.py
+++ engine/core/queries.py
@@ -1,4 +1,5 @@
 import pickle
+import shlex
 import xlrd
 from configs import dataset_path, index_file_path
 
@@ -9,11 +10,34 @@ sheet = xlrd.open_workbook(dataset_path).sheet_by_index(0)
 data_keys = ['date', 'title', 'source', 'summary', 'tags', 'content', 'thumbnail']
 
 
+def query_tokenize(q_str):
+    phrase_tokens = []
+    not_tokens = []
+    regular_tokens = []
+    for q in shlex.split(q_str):
+        if ' ' in q:
+            phrase_tokens.append(q)
+        elif '!' in q:
+            not_tokens.append(q[1:])
+        else:
+            regular_tokens.append(q)
+    regular_tokens.sort(key=lambda x: len(postings_list[x]))
+    return phrase_tokens, regular_tokens, not_tokens
+
+
 def search(q_str):
-    q_tokens = q_str.split()
-    docs = set(postings_list[q_tokens[0]])
-    for t in q_tokens[1:]:
-        docs.intersection_update(postings_list[t])
+    phrase_tokens, regular_tokens, not_tokens = query_tokenize(q_str)
+    docs = set(postings_list[regular_tokens[0]].keys())
+    for t in regular_tokens[1:]:
+        if not docs:
+            break
+        docs.intersection_update(postings_list[t].keys())
+
+    if docs:
+        for nt in not_tokens:
+            for d in postings_list[nt].keys():
+                if d in docs:
+                    docs.remove(d)
 
     results = []
     for d in docs:
