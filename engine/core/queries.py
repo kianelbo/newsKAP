c5f397d610c070b9eba0a4c8fd27da2d3e86aaec
Armin Kazemi <arminkz@live.com>
2019-12-24 22:27:33 +0330

    fixed snippet generation

--- engine/core/queries.py
+++ engine/core/queries.py
@@ -11,6 +11,60 @@ vocabulary = postings_list.keys()
 data_keys = ['date', 'title', 'source', 'summary', 'tags', 'content', 'thumbnail']
 
 
+def make_snippet2(regular_tokens ,phrase_tokens ,doc_id):
+    news_words = remove_html(sheet.cell_value(doc_id, 5)).split()
+    print("regular tokens:",regular_tokens)
+    print("phrase tokens:", phrase_tokens)
+
+    phrase_snippet_limit = 2
+    regular_snippet_limit = 6
+
+    snippet = ""
+
+    phrase_snippet = []
+    for phrase in phrase_tokens:
+        if phrase_snippet_limit == 0:
+            break
+        for i in postings_list[phrase[0]][doc_id]:
+            print(f"checking if position {i} makes a phrase ...")
+            valid_i = True
+            # check if this i represents a phrase
+            for j, phrase_word in enumerate(phrase[1:]):
+                print(f"checking position {i+j+1}")
+                if doc_id not in postings_list[phrase_word] or i + j + 1 not in postings_list[phrase_word][doc_id]:
+                    valid_i = False
+                    print("i invalid")
+                    break
+            if valid_i:
+                phrase_snippet_limit -= 1;
+                phrase_snippet += news_words[i - 5:i - 1]
+                # append phrase
+                phrase_snippet.append('<span class="bold">')
+                for h in range(0,len(phrase)):
+                    phrase_snippet.append(news_words[i+h])
+                phrase_snippet.append('</span>')
+                phrase_snippet += news_words[i + len(phrase):i + len(phrase) + 5]
+                phrase_snippet.append("... ")
+
+
+    snippet_words = []
+    for q in regular_tokens:
+        if doc_id not in postings_list[q]:
+            continue
+        for i in postings_list[q][doc_id]:
+            if regular_snippet_limit == 0:
+                break
+            regular_snippet_limit -= 1
+            snippet_words += news_words[i-5:i-1]
+            snippet_words.append('<span class="bold">' + news_words[i] + '</span>')
+            snippet_words += news_words[i + 1:i + 5]
+            snippet_words.append("... ")
+
+    snippet = ' '.join(phrase_snippet) + ' '.join(snippet_words)
+
+    return snippet
+
+
 def make_snippet(parts, doc_id):
     news_words = remove_html(sheet.cell_value(doc_id, 5)).split()
 
@@ -159,7 +213,7 @@ def search(q_str):
     results = []
     for d in docs:
         n = {'date': sheet.cell_value(d, 0)[:-7], 'title': sheet.cell_value(d, 1), 'source': sheet.cell_value(d, 2),
-             'snippet': 'snippets to be fixed', 'thumbnail': sheet.cell_value(d, 6),
+             'snippet': make_snippet2(regular_tokens,phrase_tokens,d), 'thumbnail': sheet.cell_value(d, 6),
              'relevance': compute_score(query_histogram, d), 'id': d}
         results.append(n)
     return results
